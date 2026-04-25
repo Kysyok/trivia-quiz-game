188b2aeef08dd1032279aadcc4e0b6b5921d9c07
danil <danillatency@gmail.com>
2026-04-25 15:22:24 +0300

    chore: rename session_id to player_session_token

--- backend/src/api/routes/menu.py
+++ backend/src/api/routes/menu.py
@@ -9,22 +9,22 @@ menu_router = SlowAPIRouter("Menu")
 @menu_router.route("/join")
 async def join_room(room_id, nickname):
     try:
-        player_session_id = game_engine.create_player_in_room(room_id, nickname)
+        player_session_token = game_engine.create_player_in_room(room_id, nickname)
     except JoinError as error:
         return {
             "error": error.message
         }
 
     return {
-        "session_id": player_session_id,
+        "session_token": player_session_token,
     }
 
 
 @menu_router.route("/create")
 async def create_game(nickname):
-    room_id, player_session_id = game_engine.create_room(nickname)
+    room_id, player_session_token = game_engine.create_room(nickname)
 
     return {
         "room_number": room_id,
-        "session_id": player_session_id
+        "session_token": player_session_token
     }
