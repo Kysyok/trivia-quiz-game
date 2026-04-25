8823f6cd7ff0c1b0932a845a18a1f6601647a603
danil <danillatency@gmail.com>
2026-04-25 15:25:25 +0300

    feat: make game exception hierarchy

--- backend/src/app/game/exceptions.py
+++ backend/src/app/game/exceptions.py
@@ -1,5 +1,14 @@
-class JoinError(Exception):
+class GameError(Exception):
     def __init__(self, message):
         super().__init__()
         self.add_note(message)
         self.message = message
+
+
+class JoinError(GameError):
+    def __init__(self, message):
+        super().__init__(message)
+
+class StartError(GameError):
+    def __init__(self, message):
+        super().__init__(message)
