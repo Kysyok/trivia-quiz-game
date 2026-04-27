33a83db32b5959ea66040a37334a442901b62650
danil <danillatency@gmail.com>
2026-04-27 16:45:09 +0300

    fix: make player session token json serializable

--- backend/src/app/game/player.py
+++ backend/src/app/game/player.py
@@ -4,7 +4,7 @@ import uuid
 class Player:
     def __init__(self, nickname):
         self.nickname = nickname
-        self.session_token = uuid.uuid4()
+        self.session_token = uuid.uuid4().hex
         self.score = int()
         self.answering = False
 
